c540c0ec2d053e8c443ed1d78f154eda99b79468
rahul-kumi <kayrahul@gmail.com>
2019-02-22 17:36:09 +0530

    remove debug flags

--- olclient/cli.py
+++ olclient/cli.py
@@ -91,7 +91,6 @@ def main():
     # prompt first time users to configure credentials for olclient
     try:
         if len(ia.config.get_config()) == 0:
-            print(ia.config.get_config())
             raise ValueError("No configuration set")
     except ValueError as e:
         print("Seems like you haven't configured your olclient with credentials.\n"
