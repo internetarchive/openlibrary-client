26a52372eea3d6eca4dbd265c055987d2eed335d
Guyrandy Jean-Gilles <jguyrandy@yahoo.com>
2020-10-01 18:41:41 -0700

    add dry_run_declaration(), process_row(), and setup_logger(). Patch making_a_bot example. Conform code style.

--- tests/test_bots.py
+++ tests/test_bots.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import, division, print_function
 
 import copy
+import logging
 import random
 import string
 import unittest
@@ -14,7 +15,8 @@ except ImportError:
 
 from argparse import ArgumentTypeError
 from olclient.openlibrary import OpenLibrary
-from olclient.bots import BaseBot
+from olclient.bots import AbstractBotJob
+from os import path
 
 
 class TestBots(unittest.TestCase):
@@ -26,34 +28,68 @@ class TestBots(unittest.TestCase):
         self.falsey_values = ['no', 'false', 'f', 'n', '0']
 
     def test__init__(self):
-        bot = BaseBot()
+        bot = AbstractBotJob()
         assert bot.changed == 0
         assert bot.limit == 1
         assert bot.dry_run is True
         assert bot.logger.handlers
+        assert getattr(bot, 'console_handler', None) is not None
         assert isinstance(bot.ol, OpenLibrary)
 
     def test__str2bool_returns_true_for_truthy_input(self):
         truthy_input = self.truthy_values[random.randint(0, len(self.truthy_values) - 1)]
-        bot = BaseBot(ol=self.ol)
+        bot = AbstractBotJob(ol=self.ol)
         assert bot._str2bool(truthy_input)
 
     def test__str2bool_returns_false_for_falsey_input(self):
         falsey_input = self.falsey_values[random.randint(0, len(self.falsey_values) - 1)]
-        bot = BaseBot(ol=self.ol)
+        bot = AbstractBotJob(ol=self.ol)
         assert bot._str2bool(falsey_input) is False
 
     def test__str2bool_errors_for_non_boolean_input(self):
         non_boolean_input = random.choice(string.ascii_letters)
         while non_boolean_input in self.falsey_values or non_boolean_input in self.truthy_values:
             non_boolean_input = random.choice(string.ascii_letters)
-        bot = BaseBot(ol=self.ol)
+        bot = AbstractBotJob(ol=self.ol)
         self.assertRaises(ArgumentTypeError, bot._str2bool, non_boolean_input)
+
+    def test_dry_run_declaration_when_dry_run_is_true(self):
+        bot = AbstractBotJob(dry_run=True)
+        bot.logger.info = Mock()
+        bot.dry_run_declaration()
+        bot.logger.info.assert_called_with('dry-run is TRUE. No external modifications will be made.')  # TODO
+
+    def test_dry_run_declaration_when_dry_run_is_false(self):
+        bot = AbstractBotJob(dry_run=False)
+        bot.logger.info = Mock()
+        bot.dry_run_declaration()
+        bot.logger.info.assert_called_with('dry-run is FALSE. Permanent modifications can be made.')  # TODO
+
+    def test_process_row_with_bytecode(self):
+        random_data = list()
+        for i in range(4):
+            random_data.append(string.ascii_letters[random.randint(0, len(string.ascii_letters)-1)])
+        random_data = '\t'.join([random_data[0], random_data[1], random_data[2], random_data[3], '{"foo": "bar" }'])
+        random_byte_data = random_data.encode()
+        job = AbstractBotJob()
+        returned_row, returned_json_data = job.process_row(random_byte_data)
+        assert isinstance(returned_row, list)
+        assert isinstance(returned_json_data, dict)
+
+    def test_process_row_with_string(self):
+        random_data = list()
+        for i in range(4):
+            random_data.append(string.ascii_letters[random.randint(0, len(string.ascii_letters) - 1)])
+        random_data = '\t'.join([random_data[0], random_data[1], random_data[2], random_data[3], '{"foo": "bar" }'])
+        job = AbstractBotJob()
+        returned_row, returned_json_data = job.process_row(random_data)
+        assert isinstance(returned_row, list)
+        assert isinstance(returned_json_data, dict)
 
     @patch('olclient.bots.sys.exit')
     def test_save_exits_when_limit_reached(self, mock_sys_exit):
         save_fn = Mock()
-        bot = BaseBot(ol=self.ol, dry_run=True, limit=random.randint(2, 20))
+        bot = AbstractBotJob(ol=self.ol, dry_run=True, limit=random.randint(2, 20))
         bot.logger.info = Mock()
         old_changed = copy.deepcopy(bot.changed)
         for i in range(bot.limit + 1):  # simulate calling save_fn many times in a run() method
@@ -68,7 +104,7 @@ class TestBots(unittest.TestCase):
     @patch('olclient.bots.sys.exit')
     def test_save_when_dry_run_is_false(self, mock_sys_exit):
         save_fn = Mock()
-        bot = BaseBot(ol=self.ol, dry_run=False)
+        bot = AbstractBotJob(ol=self.ol, dry_run=False)
         bot.logger.info = Mock()
         old_changed = copy.deepcopy(bot.changed)
         bot.save(save_fn)
@@ -81,7 +117,7 @@ class TestBots(unittest.TestCase):
     @patch('olclient.bots.sys.exit')
     def test_save_when_dry_run_is_true(self, mock_sys_exit):
         save_fn = Mock()
-        bot = BaseBot(ol=self.ol, dry_run=True)
+        bot = AbstractBotJob(ol=self.ol, dry_run=True)
         bot.logger.info = Mock()
         old_changed = copy.deepcopy(bot.changed)
         bot.save(save_fn)
@@ -91,3 +127,8 @@ class TestBots(unittest.TestCase):
         assert bot.changed == old_changed + 1
         assert not bot.changed > bot.limit
 
+    def test_setup_logger(self):
+        job_name = __name__
+        logger, console_handler = AbstractBotJob.setup_logger(job_name)
+        assert isinstance(logger, logging.Logger)
+        assert isinstance(console_handler, logging.StreamHandler)
