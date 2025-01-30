fe1cd36a74e3d60ab8e099468656aadb19a385c3
Mek <michael.karpeles@gmail.com>
2025-01-30 10:46:56 -0800

    change function ordering

--- olclient/openlibrary.py
+++ olclient/openlibrary.py
@@ -263,18 +263,6 @@ class OpenLibrary:
                 )
                 return r
 
-            def add_book_cover_from_url(self, cover_url, file_name="cover.jpg", mime_type="image/jpeg"):
-                response = requests.get(cover_url)
-                
-                if response.status_code != 200:
-                    raise ValueError(f"Failed to download image from {cover_url}. Status code: {response.status_code}")
-            
-                return self.add_book_cover_from_file(
-                    file_name=file_name,
-                    cover_data=response.content,
-                    mime_type=mime_type
-                )
-                
             def add_book_cover_from_file(
                     self,
                     file_name: str,
@@ -289,6 +277,18 @@ class OpenLibrary:
                 return self.OL.session.post(
                     f'{self.OL.base_url}/books/{self.olid}/-/add-cover',
                     files=form_data_body
+                )
+
+            def add_book_cover_from_url(self, cover_url, file_name="cover.jpg", mime_type="image/jpeg"):
+                response = requests.get(cover_url)
+                
+                if response.status_code != 200:
+                    raise ValueError(f"Failed to download image from {cover_url}. Status code: {response.status_code}")
+            
+                return self.add_book_cover_from_file(
+                    file_name=file_name,
+                    cover_data=response.content,
+                    mime_type=mime_type
                 )
 
             def save(self, comment):
